198578fa407bdfa4fa4b437a79f509a1b3ff3957
Julian Silerio <julian.silerio@gmail.com>
2017-11-16 01:40:12 -0500

    hw3 finish part2

--- hw3/homework3.py
+++ hw3/homework3.py
@@ -7,7 +7,7 @@ jjs2245
 import sys, string
 
 def extract_words(text):
-    return text.split('\t')[1].lower().translate('', '', string.punctuation).split(' ')
+    return text.lower().translate('', '', string.punctuation).split(' ')
     # should return a list of words in the data sample.
 
 
@@ -24,21 +24,66 @@ class NbClassifier(object):
 
     def collect_attribute_types(self, training_filename, k):
         counter = {}
-        with open(training_filename) as file:
-            for text in file.read():
-                words = extract_words(text)
-                for word in words:
-                    if word in counter.keys():
-                        counter[word] += 1
-                    else:
-                        counter[word] = 1
+        data = get_data(training_filename)
+
+        for datum in data:
+            words = datum[1]
+            for word in words:
+                if tuple() in counter.keys():
+                    counter[word] += 1
+                else:
+                    counter[word] = 1
+
         for key in counter.keys():
             if counter[key] >= k:
                 attribute_types.add(key)
 
     def train(self, training_filename):
-        self.label_prior = {} # replace this
-        self.word_given_label = {} #replace this
+        self.label_prior = {}
+        self.word_given_label = {}
+
+        data = get_data(training_filename)
+        count_label = {}
+        count_word_given_label = {}
+
+        count_label_wgl = {}
+
+        # parse data for count(labels) and count(words|label)
+        for datum in data:
+            label, text = datum
+            if label in label_prior.keys():
+                count_label[label] = 1
+            else:
+                count_label[label] += 1
+
+        # count words given label
+            words = extract_words(text)
+            for word in words:
+                if word in self.attribute_types:
+                    # wgl counts
+                    if label in count_label_wgl.keys():
+                        count_label_wgl[label] += 1
+                    else:
+                        count_label_wgl[label] = 1
+
+                    # label counts
+                    if word in count_word_given_label.keys():
+                        count_word_given_label[(word, label)] += 1
+                    else:
+                        count_word_given_label[(word, label)] = 1
+
+        # build label_prior
+        total_labels = sum(label_count.values())
+        for label in count_label.keys():
+            self.label_prior[label] = count_label[label]/total_labels
+
+        # value of c for laplacian smoothing
+        c = 1
+
+        # build word_given_label with laplacian smoothing
+        for tuple in count_word_given_label.keys():
+            word, label = tuple
+            self.label_prior[tuple] = (count_word_given_label[tuple]+c)/(count_label_wgl[label]+(c*len(self.attribute_types)))
 
     def predict(self, text):
         return {} #replace this
@@ -50,6 +95,15 @@ class NbClassifier(object):
         fscore = 0.0
         accuracy = 0.0
         return precision, recall, fscore, accuracy
+
+    def get_data(training_filename):
+        data = []
+        with open(training_filename) as file:
+            for line in file.read():
+                label, text = line.split('\t')
+                data.append(label, text)
+        return data
+
 
 
 def print_result(result):
