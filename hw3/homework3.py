18d39ee3405f83d3c6b25295242041c623f8d60a
Julian Silerio <julian.silerio@gmail.com>
2017-11-16 13:32:33 -0500

    hw3 still debugging part3

--- hw3/homework3.py
+++ hw3/homework3.py
@@ -8,17 +8,21 @@ from math import log
 import sys, string
 
 def extract_words(text):
-    return text.lower().translate(('', '', string.punctuation)).split(' ')
+    words = text.lower()
+    for c in string.punctuation:
+        words = words.replace(c,'')
+    words.replace('\n','')
+    return words.split(' ')
     # should return a list of words in the data sample.
 
 def get_data(training_filename):
     data = []
-    print("Reading in {}\n".format(training_filename))
+    print("Reading in {}".format(training_filename))
     with open(training_filename) as file:
         for line in file.readlines():
             label, text = line.split('\t')
             data.append((label, text))
-    print("Finished reading {}\n".format(training_filename))
+    print("Finished reading {}".format(training_filename))
     return data
 
 
@@ -29,20 +33,19 @@ class NbClassifier(object):
         self.attribute_types = set()
         self.label_prior = {}
         self.word_given_label = {}
-
+        self.data = get_data(training_filename)
 
         self.collect_attribute_types(training_filename, 1)
         self.train(training_filename)
 
     def collect_attribute_types(self, training_filename, k):
         counter = {}
-        data = get_data(training_filename)
 
-        print("Collecting attribute types for {}\n".format(training_filename))
-        for datum in data:
-            words = datum[1]
+        print("Collecting attribute types for {}".format(training_filename))
+        for datum in self.data:
+            words = extract_words(datum[1])
             for word in words:
-                if tuple() in counter.keys():
+                if word in counter.keys():
                     counter[word] += 1
                 else:
                     counter[word] = 1
@@ -50,24 +53,22 @@ class NbClassifier(object):
         for key in counter.keys():
             if counter[key] >= k:
                 self.attribute_types.add(key)
-
         print("Finish attribute types")
 
     def train(self, training_filename):
         self.label_prior = {}
         self.word_given_label = {}
 
-        data = get_data(training_filename)
         count_label = {}
         count_word_given_label = {}
 
         count_label_wgl = {}
 
-        print("Begin training on {}\n".format(training_filename))
+        print("Begin training on {}".format(training_filename))
         # parse data for count(labels) and count(words|label)
-        for datum in data:
+        for datum in self.data:
             label, text = datum
-            if label in self.label_prior.keys():
+            if label in count_label:
                 count_label[label] += 1
             else:
                 count_label[label] = 1
@@ -88,9 +89,9 @@ class NbClassifier(object):
                         count_word_given_label[(word, label)] += 1
                     else:
                         count_word_given_label[(word, label)] = 1
-
+        print(count_label)
         total_labels = sum(count_label.values())
-
+        print(total_labels)
         # value of c for laplacian smoothing
         c = 1
 
@@ -100,13 +101,14 @@ class NbClassifier(object):
             self.word_given_label[tuple] = count_word_given_label[tuple]/count_label_wgl[label]
 
             # build wgl for other label if not present
-            # also gonna build label_prior in here since smoothing already
+            # also gonna build label_prior in here since iterating through count_label already
             for c_label in count_label.keys():
-                self.label_prior[c_label] = count_label[c_label]/total_labels
+                self.label_prior[c_label] = count_label[c_label]/float(total_labels)
 
                 if (word, c_label) not in self.word_given_label.keys():
                     self.word_given_label[(word, c_label)] = (c)/(count_label_wgl[c_label]+(c*len(self.attribute_types)))
-        print("Finish training\n")
+
+        print("Finish training")
 
     # given the probabilities predict the likelihood of being a given label
     def predict(self, text):
@@ -132,12 +134,14 @@ class NbClassifier(object):
         tn = 0
         fn = 0
 
-        print("Evaluating {}\n".format(test_filename))
+        print("Evaluating {}".format(test_filename))
         with open(test_filename) as file:
             for line in file.readlines():
                 label, text = line.split('\t')
-                label_predict = self.predict(text)
-
+                label_candidates = self.predict(text)
+                print(label_candidates)
+                label_predict = max(label_candidates,key=label_candidates.get)
+                print("predicted:{}\nactual:{}\n".format(label_predict,label))
                 if label_predict == label:
                     if label == 'spam':
                         tp += 1
@@ -154,6 +158,7 @@ class NbClassifier(object):
         accuracy = (tp+tn)/(tp+tn+fp+fn)
 
         print("Finish evaluation\n")
+        print("TP:{} TN:{} FP:{} FN:{}".format(tp, tn, fp, fn))
         return precision, recall, fscore, accuracy
 
 
